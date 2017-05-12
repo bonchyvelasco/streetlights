7a96d75e3b7ea2fe0b62338e603b86b14babcb74
Neal Bryan Velasco <bonchyvelasco@outlook.com>
2017-05-12 21:32:30 +0800

    Added favicon.png

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,7 @@
 
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta charset="utf-8">
+        <link rel="shortcut icon" href="{{ asset('img/favicon.png') }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
